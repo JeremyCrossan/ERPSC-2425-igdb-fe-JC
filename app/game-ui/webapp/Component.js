00ba95fe4e81f803a9b93a984a908585fa2b6520
Jeremy Crossan <114576701+JeremyCrossan@users.noreply.github.com>
2025-06-04 11:14:42 +0200

    Navigeren tussen Students en Games, niet werkend.

--- app/game-ui/webapp/Component.js
+++ app/game-ui/webapp/Component.js
@@ -1,12 +1,27 @@
 sap.ui.define(
-    ["sap/fe/core/AppComponent"],
-    function (Component) {
-        "use strict";
-
-        return Component.extend("gameui.Component", {
-            metadata: {
-                manifest: "json"
-            }
-        });
+    ["sap/fe/core/AppComponent", "sap/ui/core/Component"],
+    function (AppComponent, Component) {
+      "use strict";
+  
+      return AppComponent.extend("gameui.Component", {
+        metadata: {
+          manifest: "json",
+        },
+  
+        init: function () {
+          AppComponent.prototype.init.apply(this, arguments);
+  
+          // Zorg dat de juiste taal gezet wordt op het i18n-model
+          const sLang = sap.ui.getCore().getConfiguration().getLanguage();
+          const oModel = this.getModel("i18n");
+          if (oModel && oModel.enhance) {
+            oModel.enhance({
+              bundleName: "gameui.i18n.i18n",
+              bundleLocale: sLang
+            });
+          }
+        }
+      });
     }
-);
+  );
+  
